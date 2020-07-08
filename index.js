2335ff20d79dfaa3c9d4c05bc1875e13aabb5ce7
chandraoke <oke.18421037@student.ubl.ac.id>
2020-07-08 11:12:35 +0700

    Signed-off-by: chandraoke <oke.18421037@student.ubl.ac.id>

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const mongoURL = 'mongodb://localhost:27017/dbcandra'
+mongoose.connect(mongoURL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('berhasil konek kedatabase')
+}).catch(() => {
+  console.log('gagal konek ke database')
+})
+
+const directory = path.join(__dirname, '/statics/')
+app.use(express.static(directory))
+app.use(cors())
+
+app.use(bodyParser.json({
+  extended: true,
+  limit: '20mb'
+}))
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: '20mb'
+}))
+
+app.use('/user', require('./routes/User'))
+app.use('/movie', require('./routes/Movie'))
+app.use('/order', require('./routes/Order'))
+
+app.listen(5000, function () {
+  console.log('Server Telah Berjalan Di port 5000')
+})
